cb7b23bb56c5a888764af2b39acd8aa87770122b
Shana <903443276@qq.com>
2025-05-04 01:59:41 -0500

    Update __init__.py

--- python_src/__init__.py
+++ python_src/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0'
+__version__ = '1.01'
 
 def version():
     return __version__
@@ -8,10 +8,10 @@ from .database import get_tables
 
 try:
     import snappy
-    table_dict = snappy.database.add_tables_from_package('ten_tet_census', False)
+    table_dict = snappy.database.add_tables_from_package('snappy_10_tets', False)
     for name, table in table_dict.items():
         setattr(snappy, name, table)
         if name not in snappy.database_objects:
             snappy.database_objects.append(name)
 except ImportError:
-    print('er')
+    raise RuntimeError('Error happened when loading 10 tet census data')
